324e4745f3a3de876fe2a2e7905f0c424f59ed4c
theogoulart <theo_goulart@hotmail.com>
2019-10-08 19:33:21 -0300

    use express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.write('oi cara');
+    res.end();
+});
+
+app.listen(5050, () => { console.log('listening to port 5050'); });
